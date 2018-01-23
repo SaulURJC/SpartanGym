4913b59c14ba3e5d28a8aa8acabe6cd9d0da27bd
unknown <es.loachamin@alumnos.urjc.es>
2018-01-23 19:52:33 +0100

    Commit inicial

---
+++ src/main/java/spartans/SpartanGym/SpartanGymApplication.java
@@ -0,0 +1,12 @@
+package spartans.SpartanGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanGymApplication.class, args);
+	}
+}
